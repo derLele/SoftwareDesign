c5cce1354144958c25edf7e0cac5d0bf2a8e5b92
derLele <44344634+derLele@users.noreply.github.com>
2020-05-13 10:59:06 +0200

    Update main.ts

--- L02/main.ts
+++ L02/main.ts
@@ -1,1 +1,2 @@
-console.log("Hello World! :D");
+console.log("Hello World! :D"); //lmao was das für ein dreck?!? hätte man auch auf du guten 20 zeilen strecken können.
+                                // auch fehlen komplett die Kommentare. so geht das nicht *fuchtelt mit der Faust in der Luft*
